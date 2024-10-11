b75a31559930509fd25c4c374688779cf4fad692
mal2017 <>
2024-10-11 12:41:15 -0400

    update with new Flybase, multiple formats for anki import

--- FlybaseAnki.R
+++ FlybaseAnki.R
@@ -1,21 +1,32 @@
 library(tidyverse)
 
-snapshots <- read_tsv("http://ftp.flybase.org/releases/FB2022_02/precomputed_files/genes/gene_snapshots_fb_2022_02.tsv.gz",skip = 4)
+# creates anki deck in csv format per following spec
+# https://docs.ankiweb.net/importing/text-files.html
+
+snapshots <- read_tsv("https://ftp.flybase.net/releases/current/precomputed_files/genes/gene_snapshots_fb_2024_04.tsv.gz",skip = 4)
 snapshots <- dplyr::rename(snapshots, gene_id = `##FBgn_ID`)
 
 
-gene_summaries <- read_tsv("http://ftp.flybase.org/releases/FB2022_02/precomputed_files/genes/automated_gene_summaries.tsv.gz",skip = 1)
+gene_summaries <- read_tsv("https://ftp.flybase.net/releases/current/precomputed_files/genes/automated_gene_summaries.tsv.gz",skip = 1)
 gene_summaries <- dplyr::rename(gene_summaries, gene_id = `#primary_FBgn`)
 
 res0 <- inner_join(snapshots, gene_summaries, by="gene_id")
 
+res <- res0 |> dplyr::select(GeneSymbol,GeneID=gene_id,GeneName,Snapshot=gene_snapshot_text,Summary=summary_text) |>
+  distinct()
 
-res %>%
-  dplyr::select(GeneSymbol,gene_snapshot_text) %>%
-  filter(gene_snapshot_text!="Contributions welcome.") %>%
-  distinct() %>%
-  write_csv("snapshots.csv", col_names = F)
 
+# make header
+h <- list(c(paste0("#columns:",paste(colnames(res),collapse=",")),
+  "#deck:Flybase_2024_04",
+  "#separator:Comma"))
+names(h) <- colnames(res)[1]
+
+bind_rows(res,as_tibble(h)) |> arrange(-str_detect(GeneSymbol,"^#")) |>
+  write_csv("deck_with_headers.csv.gz", col_names = F)
+
+
+write_csv(res,"deck_no_headers.csv.gz", col_names = F)
 
 
 
